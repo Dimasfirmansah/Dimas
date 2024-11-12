8c1688571a24373a843176cbf2dbd6775a95f535
Dimas <dfir2492@gmail.com>
2024-11-12 21:10:27 +0700

    Add files via upload

--- mikrotik.sh
+++ mikrotik.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
+#!/usr/bin/expect -f
 
-# Set variables
-MIKROTIK_USER="admin"
-MIKROTIK_PASS="123"
-MIKROTIK_IP="192.168.9.10"
+set MIKROTIK_USER "admin"
+set MIKROTIK_PASS "123"
+set MIKROTIK_IP "192.168.9.10"
 
-# Telnet login and configuration
-echo "$MIKROTIK_PASS" | telnet $MIKROTIK_IP << EOF
-/interface dhcp-client add interface=ether1 disabled=no
-/interface address add address=192.168.200.1/24 interface=ether2
-/interface dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool disabled=no
-/ip pool add name=dhcp_pool ranges=192.168.200.10-192.168.200.100
-/ip dhcp-server network add address=192.168.200.0/24 gateway=192.168.200.1
-/ip route add gateway=192.168.9.1
-EOF
+spawn telnet $MIKROTIK_IP
+expect "login:"
+send "$MIKROTIK_USER\r"
+expect "Password:"
+send "$MIKROTIK_PASS\r"
+expect ">"
+send "/interface dhcp-client add interface=ether1 disabled=no\r"
+send "/interface address add address=192.168.200.1/24 interface=ether2\r"
+send "/interface dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool disabled=no\r"
+send "/ip pool add name=dhcp_pool ranges=192.168.200.10-192.168.200.100\r"
+send "/ip dhcp-server network add address=192.168.200.0/24 gateway=192.168.200.1\r"
+send "/ip route add gateway=192.168.9.1\r"
+send "exit\r"
 
-echo "MikroTik configuration completed."
+expect eof
