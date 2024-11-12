e58fcfc6a02d48b55c89911527b8e9366b9f1bad
Dimas <dfir2492@gmail.com>
2024-11-12 20:55:40 +0700

    Add files via upload

--- mikrotik.sh
+++ mikrotik.sh
@@ -1,15 +1,18 @@
-# Remote Configuration for MikroTik
-echo "Configuring MikroTik device..."
+#!/bin/bash
+
+# Set variables
 MIKROTIK_USER="admin"
 MIKROTIK_PASS="123"
-MIKROTIK_IP="192.168.9.10"  # Replace with MikroTik's IP in VLAN 10
+MIKROTIK_IP="192.168.200.1"
 
-sshpass -p "$MIKROTIK_PASS" ssh -t -o StrictHostKeyChecking=no $MIKROTIK_USER@$MIKROTIK_IP "bash -l << EOF
-/ip dhcp-client add interface=ether1 disabled=no
-/ip address add address=192.168.200.1/24 interface=ether2
-/ip dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool disabled=no
+# Telnet login and configuration
+echo "$MIKROTIK_PASS" | telnet $MIKROTIK_IP << EOF
+/interface dhcp-client add interface=ether1 disabled=no
+/interface address add address=192.168.200.1/24 interface=ether2
+/interface dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool disabled=no
 /ip pool add name=dhcp_pool ranges=192.168.200.10-192.168.200.100
 /ip dhcp-server network add address=192.168.200.0/24 gateway=192.168.200.1
 /ip route add gateway=192.168.9.1
-EOF"
-echo "MikroTik completed."
+EOF
+
+echo "MikroTik configuration completed."
