8efc5c66771bee7ce645d9f9f83f69a1f97dfe42
Dimas <dfir2492@gmail.com>
2024-11-18 05:30:08 +0700

    Add files via upload

--- mikrotik.sh
+++ mikrotik.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-set MIKROTIK_USER "admin"
-set MIKROTIK_PASS "123"
-set MIKROTIK_IP "192.168.9.11"
+MIKROTIK_USER="admin"
+MIKROTIK_PASS="123"
+MIKROTIK_IP="192.168.9.11"
 
+expect << EOF
 spawn telnet $MIKROTIK_IP
 expect "login:"
 send "$MIKROTIK_USER\r"
@@ -11,11 +12,17 @@ expect "Password:"
 send "$MIKROTIK_PASS\r"
 expect ">"
 send "/interface dhcp-client add interface=ether1 disabled=no\r"
+expect ">"
 send "/interface address add address=192.168.200.1/24 interface=ether2\r"
-send "/interface dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool disabled=no\r"
+expect ">"
 send "/ip pool add name=dhcp_pool ranges=192.168.200.10-192.168.200.100\r"
+expect ">"
+send "/ip dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool disabled=no\r"
+expect ">"
 send "/ip dhcp-server network add address=192.168.200.0/24 gateway=192.168.200.1\r"
+expect ">"
 send "/ip route add gateway=192.168.9.1\r"
+expect ">"
 send "exit\r"
-
-expect eof
+expect eof
+EOF
